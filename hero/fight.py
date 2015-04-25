8c5cac8dfedd2f8cf476c5a12d218973b9729952
denis-pinaev <corcc@yandex.ru>
2015-04-25 13:29:53 +0300

    fg hdfgh dfh

--- hero/fight.py
+++ hero/fight.py
@@ -82,7 +82,7 @@ if person in [2,3,4]:
     not_attack = {"user":[user_not_attack], "orden":[orden_Dark, [{'name':u'Лемберг'}]]}
 
 if person in [5,17,11]:
-    not_attack = {"user":[], "orden":[orden_Dark, [{'name':u'Анархия тьмы'},{'name':u'Элита Медведей'},{'name':u'МЕДВЕДИ'},{'name':u'Академия Медведей'},{'name':u'Сильные Медведи'},{'name':u'Легион Медведей'}]]}
+    not_attack = {"user":[], "orden":[orden_Dark, [{'name':u'Анархия тьмы'},{'name':u'Элита Медведей'},{'name':u'МЕДВЕДИ'},{'name':u'Академия Медведей'},{'name':u'Сильные Медведи'},{'name':u'Легион Медведей'},{'name':u'Бурые Медведи'},{'name':u'МЫ ВМЕСТЕ'}]]}
 
 if person in [6]:
     not_attack = {"user":[], "orden":[orden_Dark, [{'name':u'Украина'}]]}

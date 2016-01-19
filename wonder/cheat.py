e2b0339f55c10a0d4546663a267250d5f88c4496
denis-pinaev <corcc@yandex.ru>
2016-01-19 18:03:27 +0300

    dghdfg hfg h

--- wonder/cheat.py
+++ wonder/cheat.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 def read(fname):
     f = open(fname, 'rb')
@@ -43,52 +44,45 @@ prev_type = ""
 pca = "miningRunes"
 prev_x = 1
 prev_y = 1
-dangerTypes = ["ballista", "catapulta", "tower", "electricThrower", "flamethrower", "elementalTower"]
+dangerTypes = ["ballista", "catapulta", "tower", "electricThrower", "flamethrower", "elementalTower", "ballistaGoblin", "catapultaGoblin", "towerGoblin", "electricThrowerGoblin", "flamethrowerGoblin", "elementalTowerGoblin"]
 dangerCoords = [(0, 16), (0, 16), (0, 16), (16, 0), (33, 0), (33, 0), (33, 16), (33, 33)]
 nextD = 0
 maxD = 3
 for item in m:
-    if nextD<maxD: setType(item, "catapulta", 1)
     t, level = getType(item)
-    if t not in ["wall", "tree", "stone"]: print json.dumps(item)
-    if prev_type != t:
-        prev_type = t
-    #    prev_x, prev_y = getCoords(item)
-    #else:
-    #    setCoords(item, prev_x, prev_y)
-    #newType = changeType(prev_type)
-    #setType(item, newType, 1)
+    wasS = json.dumps(item)
+    if t in ["wall", "tree", "stone"]: setCoords(item, 36, 36); continue
     if t in dangerTypes:
-        #setType(item, t, 1)
-        x, y = dangerCoords[nextD]
-        nextD += 1
-        if nextD>=len(dangerCoords): nextD = 0
-        setCoords(item, x, y)
-    else: setCoords(item, 3, 16)
-    if t in ["wall", "tree", "stone"]:
-        #setType(item, t, 1)
-        setCoords(item, 36, 36)
-    if item.has_key("units"): del item["units"]
-    if not ("mining" in t):
-        setType(item, t, 1)
-        #if True:
-            #item.has_key("stored"):
-            #item["stored"] = 99999
+        setType(item, "catapulta", 1)
+        setCoords(item, 0, 16)
     else:
-        setType(item, t, 9)
-#        if item.has_key("stored"):
-#            for p in item["stored"]: item["stored"][p] = 99999
-#        if str(item["id"])=="64":
-#            setType(item, "miningGold", 9)
-    if nextD>maxD:
-        setType(item, pca, 9)
-        if pca == "miningRunes": pca = "miningGold"; setCoords(item, 3, 16)
-        elif pca == "miningGold": pca = "miningWood"; setCoords(item, 3, 16)
-        elif pca == "miningWood": pca = "miningRunes"; setCoords(item, 3, 16)
-        nextD = maxD
-        
-    #if not t in ["wall", "tree", "stone"]: setCoords(item, 15, 10)
+        if "townhall" in t: setType(item, t, 10)
+        setCoords(item, 3, 16)
+    if item.has_key("units"): del item["units"]
+    print wasS, getType(item)
 
+cata_count = 1
+wood_count = 1
+gold_count = 1
+runa_count = 1
+if len(sys.argv)>1: cata_count = int(sys.argv[1])
+if len(sys.argv)>2: wood_count = int(sys.argv[2])
+if len(sys.argv)>3: gold_count = int(sys.argv[3])
+if len(sys.argv)>4: runa_count = int(sys.argv[4])
+
+
+nid = len(m)
+for i in range(cata_count):
+    m.append({"y": 16, "x": 0, "state": 0, "type": "catapulta_1", "id": nid+i+1})
+nid = len(m)
+for i in range(wood_count):
+    m.append({"y": 16, "x": 0, "state": 0, "type": "miningWood_10", "id": nid+i+1})
+nid = len(m)
+for i in range(gold_count):
+    m.append({"y": 16, "x": 0, "state": 0, "type": "miningGold_10", "id": nid+i+1})
+nid = len(m)
+for i in range(runa_count):
+    m.append({"y": 16, "x": 0, "state": 0, "type": "miningRunes_10", "id": nid+i+1})
     
 jdata["result"]["data"]["map"]["field"] = sorted(m, key=lambda t: t["id"])
 write("in", json.dumps(jdata))

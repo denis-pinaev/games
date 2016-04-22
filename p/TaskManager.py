c57d45bbf9c9f2be4b4f21d4c235be37c427f72b
denis-pinaev <corcc@yandex.ru>
2016-04-22 19:48:29 +0300

    dfg sdfg sdfg

--- p/TaskManager.py
+++ p/TaskManager.py
@@ -1,6 +1,30 @@
 from concurrent.futures import ThreadPoolExecutor, Future
 import threading
 import datetime
+import os
+import uuid
+import random
+import zipfile
+
+class TimeCounter():
+    start_time = datetime.datetime.now()
+    finish_time = datetime.datetime.now()
+    
+    def __init__(self):
+        self.start_time = datetime.datetime.now()
+        
+    def start(self):
+        self.start_time = datetime.datetime.now()
+        
+    def uptime(self):
+        return self.finish_time - self.start_time
+    
+    def stop(self):
+        self.finish_time = datetime.datetime.now()
+        return self.uptime()
+    
+    def __unicode__(self):
+        return str(self.uptime())
 
 class TaskExecutor():
     
@@ -9,12 +33,15 @@ class TaskExecutor():
     result = Future()
     concurrency = 10
     task = lambda _: None
-    on_fail=lambda _: None
+    on_fail = lambda _: None
     iterator = iter([])
+    available = False
     
     def __init__(self, concurrency=10):
         self.concurrency = concurrency
-        
+        self.executor = ThreadPoolExecutor(concurrency)
+        self.available = True
+
     def submit(self):
         try:
             obj = next(self.iterator)
@@ -23,9 +50,9 @@ class TaskExecutor():
         if self.result.cancelled():
             return
         self.result.stats['delayed'] += 1
-        self.future = self.executor.submit(self.task, obj)
-        self.future.obj = obj
-        self.future.add_done_callback(self.upload_done)
+        future = self.executor.submit(self.task, obj)
+        future.obj = obj
+        future.add_done_callback(self.upload_done)
 
     def upload_done(self, future):
         with self.io_lock:
@@ -38,15 +65,17 @@ class TaskExecutor():
             self.result.set_result(self.result.stats)
 
     def cleanup(self, _):
+        self.available = False
         with self.io_lock:
             self.executor.shutdown(wait=False)
             
     def run(self, task, iterator, on_fail=lambda _: None):
+        if not self.available: raise Exception("Executor is not available")
+        
         self.iterator = iterator
         self.task = task
         self.on_fail = on_fail
 
-        self.executor = ThreadPoolExecutor(self.concurrency)
         self.result = Future()
         self.result.stats = {'done': 0, 'delayed': 0}
         self.result.add_done_callback(self.cleanup)
@@ -57,40 +86,104 @@ class TaskExecutor():
 
         return self.result
 
-class TestIterator():
-    array = []
-    counter = 0
-    
-    def __init__(self, array):
-        self.array = array
-        
-    def __iter__(self):
-        return self
-    
-    def next(self):
-        if len(self.array) <= self.counter:
-            raise StopIteration
+
+def read_file(file_name):
+    f = open(file_name, "r")
+    data = f.read()
+    f.close()
+    return data
+
+def save_file(data, file_name):
+    f = open(file_name, "w")
+    f.write(data)
+    f.close()
+
+def rm_rf(d):
+    for path in (os.path.join(d, f) for f in os.listdir(d)):
+        if os.path.isdir(path):
+            rm_rf(path)
         else:
-            self.counter += 1
-            return self.array[self.counter-1]
+            os.unlink(path)
+    os.rmdir(d)
+
+def get_unique_sting():
+    return uuid.uuid4().hex
+
+def get_random_int(base=100):
+    return random.randint(0,base)
+
+def get_random_string(length=10):
+    number = '0123456789'
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    id = ''
+    for _ in range(0, length, 2):
+        id += random.choice(number)
+        id += random.choice(alpha)
+    return id
+
+def generate_lines(times = 1):
+    lines = ""
+    sample_line = "<object name='%s'/>"
+    for _ in xrange(times):
+        lines += sample_line % get_random_string(10)
+    return lines
         
-def test_task(obj):
-    None#print "d", obj
+def generate_xml_file():
+    xml_text = read_file("xml_sample.txt")
+    params = {
+        "unique_string" : get_unique_sting(),
+        "random_int" : get_random_int(100),
+        "random_lines" : generate_lines(random.randint(1,10))
+    }
+    xml_text = xml_text.format(**params) 
+    return xml_text
+
+def generate_save_files(direcory="./", count=10):
+    if not os.path.exists(direcory):
+        os.mkdir(direcory)
+    for i in xrange(count):
+        file_name = "%stest_xml_%03d.xml" % (direcory, i)
+        save_file(generate_xml_file(), file_name)
+
+def generate_zip_file(file_count=1):
+    try:
+        if not os.path.exists(tmp_file_path):
+            os.mkdir(tmp_file_path)
+        directory = "%stmp_%02d/" % (tmp_file_path, file_count)
+        generate_save_files(directory, 10)
+        file_name = "%szip_%02d.zip" % (tmp_file_path, file_count)
+        zip_file = zipfile.ZipFile(file_name, "w")
+        for d, dirs, files in os.walk(directory):
+            for f in files:
+                path = os.path.join(d, f)
+                zip_file.write(path,f)
+        zip_file.close()
+    except Exception as e:
+        print type(e), e
+        
+def create_zip_files():
+    global timer
+    if os.path.exists(tmp_file_path):
+        timer = TimeCounter()
+        rm_rf(tmp_file_path)
+        print timer.stop(), '- clear old files'
+    timer = TimeCounter()
+    run_tasks(generate_zip_file, iter(xrange(100)), create_zip_files_final_callback)
     
-def printX(_):
-    print "e", _
+def create_zip_files_final_callback(r):
+    global timer
+    print timer.stop(), r.stats["done"], "zip files created"
+
+def run_tasks(task, iterator, final_callback=lambda _:None):
+    r = TaskExecutor(10).run(task, iterator)
+    while True:
+        try:
+            r.result()
+        except: None
+        if r.stats["delayed"] == 0:
+            final_callback(r)
+            break
     
-def end(r):
-    print r.result()
-    finish_time = datetime.datetime.now()
-    print (finish_time - start_time)
-    print finish_time 
-#task_queue(test_task, TestIterator([1,3,5,6,7,8,9,"s"]), 10, lambda x: printX(x))
-#r = task_queue(test_task, TestIterator(xrange(200)), 10, lambda x: printX(x))
-start_time = datetime.datetime.now()
-print start_time
-te = TaskExecutor(100)
-r = te.run(test_task, TestIterator(xrange(20000)))
-print r.result()
-r = te.run(printX, TestIterator(xrange(200)))
-print r.result()
+tmp_file_path = "./tmp2/"
+create_zip_files()
+#rm_rf(tmp_file_path)
